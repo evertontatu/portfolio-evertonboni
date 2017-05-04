2638c82f3c799b52308e8b7635a2541626225ddc
evertontatu <evertonferreira.de@gmail.com>
2017-05-04 14:34:24 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+var concat      = require('gulp-concat');
+var uglify      = require('gulp-uglify');
+var minifyCss   = require('gulp-minify-css');
+var imageop = require('gulp-image-optimization');
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+/**
+ * Build the Jekyll Site
+ */
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+/**
+ * Rebuild Jekyll & do page reload
+ */
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+/**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', ['sass', 'scripts', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        }
+    });
+});
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+  return gulp.src('_sass/main.scss')
+      .pipe(sass({
+          includePaths: ['scss'],
+          onError: browserSync.notify
+      }))
+      .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+      .pipe(minifyCss())
+      .pipe(gulp.dest('_site/css'))
+      .pipe(browserSync.reload({stream:true}))
+      .pipe(gulp.dest('css'));
+});
+
+gulp.task('scripts', function () {
+  gulp.src(['js/**/*.js'])
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(browserSync.reload({stream:true}))
+    .pipe(gulp.dest('_site/js'));
+});
+
+gulp.task('images', function(cb) {
+    gulp.src(['images/**/*.png','images/**/*.jpg','images/**/*.gif','images/**/*.jpeg']).pipe(imageop({
+        optimizationLevel: 5,
+        progressive: true,
+        interlaced: true
+    })).pipe(gulp.dest('_site/images/images')).on('end', cb).on('error', cb);
+});
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
+gulp.task('watch', function () {
+  gulp.watch('_sass/*.scss', ['sass']);
+  gulp.watch('js/**/*.js', ['scripts']);
+  gulp.watch('images/**', ['images']);
+  gulp.watch(['*.html','_layouts/*.html', '_posts/*', '_includes/*.html','_includes/svg/*.svg'], ['jekyll-rebuild']);
+});
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
---
+++ js/script.js
@@ -0,0 +1,196 @@
+//MENU NAV
+$(function() {
+    $('.menu-mobile').click(function(e) {
+        e.stopPropagation();
+        toggleNav();
+    });
+
+    $('body, .navMobile > ul').click(function(e){
+        var target = $(e.target);
+        if($('#nav').hasClass('navMobile')) toggleNav();
+    });
+
+    function toggleNav(){
+        if($('#nav').hasClass('navMobile')){
+            $('#nav').removeClass('navMobile');
+        }else{
+            $('#nav').addClass('navMobile');
+        }
+    }
+});
+
+//Menu scroll
+$(function() {
+    var initialPosition = $(document).scrollTop();
+    if (initialPosition > '80') {
+        $('#menu').addClass('nav-scroll');
+        $('nav ul li').addClass('colorLink');
+        $('.menu-button-burger').addClass('greenBurger');
+        $('.contactHeader').addClass('bg-color-contact-header');
+    }
+
+    $(document).scroll(function() {
+        var initialPosition = $(document).scrollTop();
+
+        if (initialPosition > '80') {
+            $('#menu').addClass('nav-scroll');
+            $('nav ul li').addClass('colorLink');
+            $('.menu-button-burger').addClass('greenBurger');
+            $('.contactHeader').addClass('bg-color-contact-header');
+        }
+        if (initialPosition < '80') {
+            $('#menu').removeClass('nav-scroll');
+            $('nav ul li').removeClass('colorLink');
+            $('.menu-button-burger').removeClass('greenBurger');
+            $('.contactHeader').removeClass('bg-color-contact-header');
+        }
+
+    });
+});
+
+//Menu header button burger
+$(function(){
+
+	$('.menu-button-burger').click(function(e){
+		e.stopPropagation();
+		toggleNav();
+	});
+
+	$('.main').click(function(e){
+		var target = $(e.target);
+		if(!target.closest('.menu-list').length && $('.main').hasClass('show-nav')) toggleNav();
+	});
+
+	function toggleNav(){
+		if($('.main').hasClass('show-nav')){
+			$('.main').removeClass('show-nav');
+		}
+		else{
+			$('.main').addClass('show-nav');
+		}
+
+    if($('.menu-list').hasClass('show-nav')){
+      $('.menu-list').removeClass('show-nav');
+    }
+    else{
+      $('.menu-list').addClass('show-nav');
+    }
+	}
+
+});
+
+//Carousel
+$(document).ready(function() {
+  var owl = $("#slider-carousel");
+  owl.owlCarousel({
+    items: 3,
+    itemsDesktop: [1000, 3],
+    itemsDesktopSmall: [900, 2],
+    itemsTablet: [600, 1],
+    itemsMobile: false,
+    pagination: false
+  });
+  $(".next").click(function() {
+    owl.trigger('owl.next');
+  })
+  $(".prev").click(function() {
+    owl.trigger('owl.prev');
+  })
+});
+
+//Height device
+$(function() {
+    var heightDevice = $(window).height();
+    var widthDevice = $(window).width();
+
+    if(widthDevice < "1020"){
+        $('.parallax-home').css('height', heightDevice);
+        $('.parallax-container').css('height', heightDevice);
+    }
+});
+
+//Parallax
+$(function(){
+    $(window).scroll(function(){
+        var wScroll = $(this).scrollTop();
+
+        $('.parallax-text').css({
+            'transform' : 'translate(0px, '+ wScroll / 6 +'%)'
+        });
+
+        $('.parallax-text-filter').css({
+            'transform' : 'translate(0px, '+ wScroll / 6 +'%)'
+        });
+
+        $('.parallax-home').css({
+            'background-position' : 'center '+ wScroll / - 5 +'px'
+        });
+
+        $('.parallax-filter').css({
+            'background-position' : 'center '+ wScroll / - 5 +'px'
+        });
+
+    });
+});
+
+//button filtrar sectionFilter
+$(function(){
+  $('#btn-filter-mobile').click(function(){
+    $('.container-filter').toggle('show-container-filter');
+  });
+});
+
+//Tabs on product-infos
+$('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+    var $this = $(this),
+        tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+    others.removeClass('active');
+    $this.addClass('active');
+    $(tabgroup).children('div').hide();
+    $(target).show();
+})
+
+//Scrool anchor
+$(document).ready(function(){
+    $('a.anchor[href^="#"]').bind('click.smoothscroll',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top-90
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+//Mask form
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,"");
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+    return v;
+}
+function id( el ){
+    return document.getElementById( el );
+}
+window.onload = function(){
+    id('telefone').onkeyup = function(){
+        mascara( this, mtel );
+    }
+}
